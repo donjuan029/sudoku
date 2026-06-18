03acafe31df409eceff6eafd24ef2216f226719a
Juan Carlo <141068249+donjuan029@users.noreply.github.com>
2026-06-18 14:42:30 -0300

    Update GameStatusEnum.java

--- src/br/com/dio/model/GameStatusEnum.java
+++ src/br/com/dio/model/GameStatusEnum.java
@@ -6,7 +6,7 @@ public enum GameStatusEnum {
     INCOMPLETE("incompleto"),
     COMPLETE("completo");
 
-    private String label;
+    private final String label;
 
     GameStatusEnum(final String label){
         this.label = label;
